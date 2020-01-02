231842cb00b14b63ed96654985e1e78e1d144973
Lucas Scaramelo <lucas.scaramelo@FROMTIS.LOCAL>
2020-01-02 17:54:57 -0300

    insert file

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+      <div>
+          <div class="text-center">
+              <h1>Name</h1>
+          </div>
+      </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+}
+</script>
+
+<style>
+
+</style>
